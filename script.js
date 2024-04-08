d92bae202ef4fc1a5c8e53a5931986e7596522b3
shailendrapratap01 <shailendrapratapsingh@innotechteam.in>
2024-04-08 11:46:32 +0530

    functionality add and edge cases

--- script.js
+++ script.js
@@ -1,50 +1,94 @@
-const inputField = document.querySelector(".input-field");
-const keys = document.querySelectorAll(".key");
+const input = document.querySelector(".input-field");
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const equalsButton = document.querySelector(".evaluate");
+const clearAll = document.querySelector(".clear-input");
+const decimal = document.querySelector(".decimal");
 
-let inputText = [];
-const keysArray = Array.from(keys);
-inputField.value = 0;
+let expression = [];
+let checkDecimal = [];
 
-let number = [];
+numbers.forEach((number)=>{
+    number.addEventListener('click', (e)=>{
+        expression.push(e.target.innerText);
+        checkDecimal.push(e.target.innerText);
+        input.value = expression.join("");
+    })
+})
 
-for (let i = 0; i < keysArray.length; i++) {
-    keysArray[i].addEventListener("click", () => {
-        switch (keysArray[i].innerHTML) {
-            case "=": {
-                evaluation();
-                break;
+decimal.addEventListener('click',(e)=>{
+    //to convert "." to "0." in the begining 
+    if ( expression.length === 0 ) {
+        expression.push("0", ".");
+        input.value = expression.join("");
+        checkDecimal = ["0", "."];
+    }
+    // to prevent two decimals in one number
+    else if ( !(checkDecimal.includes(".")) ) {
+        checkDecimal.push(e.target.innerHTML);
+        expression.push(e.target.innerHTML);
+        input.value = expression.join("");
+    }
+})
+
+operations.forEach((operation)=>{
+    operation.addEventListener('click', (e)=>{
+        //edge cases of operators
+        if (//check for two consecutive operator
+            !( "+,*,/".includes(e.target.innerHTML) && "+,-,*,/".includes(expression[expression.length - 1])) &&
+
+            //check for operator in begining
+            !( expression.length === 0 && "+,*,/,".includes(e.target.innerHTML)) &&
+            
+            //to prevent two consicutive "-"
+            !( e.target.innerHTML === "-" && expression[expression.length - 1] === "-")
+
+            ){
+                //clear checkdecimal for new number
+                if ("+,-,*,/".includes(e.target.innerHTML)) {
+                checkDecimal = [];
             }
-            case "C": {
-                inputText = [];
-                number = [];
-                inputField.value = "";
-                break;
-            }
-            default: {
-                if("+,-,*,/".includes(keysArray[i].innerHTML)) {
-                number = [];
-                }
-                number.push(keysArray[i].innerHTML);
-                inputText.push(keysArray[i].innerHTML);
-                inputField.value = inputText.join("");
-                console.log(inputText)
-                console.log(number)
-            }
+            //it will work on normal button click when there is no edge case
+            
+            expression.push(e.target.innerText);
+            input.value = expression.join("");
         }
-    });
-}
+    })
+})
 
-const evaluation = () => {
-    const inputString = inputField.value;
-    let result = calculation(convertStringToArray(inputString));
-    inputField.value = result;
-    console.log(result)
+equalsButton.addEventListener("click", ()=>{
+    evaluate();
+})
+
+clearAll.addEventListener('click',()=>{
+    input.value = "";
+    expression = [];
+    checkDecimal = [];
+})
+
+const evaluate = () => {
+    const inputArray = input.value;
+    result = calculation(convertStringToArray(inputArray));
+
+    if (result?.toString().includes(".")) {
+        //limiting the result upto 2 decimal places
+        result = result.toFixed(2);
+        checkDecimal = ["."];
+    }
+    
+    input.value = result;
+    expression = [result];
+
+    if (!result && result !== 0) {
+        input.value = "NaN";
+        expression = [];
+    }
 };
   
-function convertStringToArray(inputString) {
+function convertStringToArray(inputArray) {
     const result = [];
     let number = "";
-    for (const character of inputString) {
+    for (const character of inputArray) {
         if ("*/+-".includes(character)) {
             if (number === "" && character === "-") {
                 number = "-";
@@ -53,7 +97,8 @@ function convertStringToArray(inputString) {
                 result.push(parseFloat(number), character);
                 number = "";
             }
-        }else {
+        } 
+        else {
             number += character;
         }
     }
@@ -62,38 +107,37 @@ function convertStringToArray(inputString) {
     }
     return result;
 }
-
+  
 function calculation(array) {
-    const precedenceArray = [
-        {
-            "/": (a, b) => a / b,
-        },
-        { 
-            "*": (a, b) => a * b 
-        },
-        { 
-            "+": (a, b) => a + b, 
-            "-": (a, b) => a - b 
-        },
+    const PrecedenceArray = [
+      {
+        "/": (a, b) => a / b,
+      },
+      { 
+        "*": (a, b) => a * b 
+      },
+      { 
+        "+": (a, b) => a + b, "-": (a, b) => a - b 
+      },
     ];
-    
-    let operator;
-    
-    for (const operators of precedenceArray) {   
-        const newarray = [];
+    //this will store operator function according to the key
+    let operatorFunction;
+    //operation according to the precedenceArray
+    for (const operators of PrecedenceArray) {   
+        const ansArray = [];
         for (const element of array) {  
             if (element in operators) {
-                operator = operators[element];
+                operatorFunction = operators[element];
             } 
-            else if (operator) {
-                newarray[newarray.length - 1] = operator(newarray[newarray.length - 1], element);
-                operator = null;
+            else if (operatorFunction) {
+                ansArray[ansArray.length - 1] = operatorFunction(ansArray[ansArray.length - 1],element);
+                operatorFunction = null;
             } 
-            else{
-                newarray.push(element);
+            else {
+                ansArray.push(element);
             }
         }
-        array = newarray;
+        array = ansArray;
     }
-    return array[0]                               
-} 
+    return array[0]
+}
